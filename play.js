b4b35316e2a0cd138c96992ca0fb759d6e015579
jeffreysalewis <jeffreysalewis@gmail.com>
2023-03-08 20:14:28 -0600

    added game background js

--- play.js
+++ play.js
@@ -1,2 +1,23 @@
 var gameBoard, player, enemy, laser, laser2;
 enemy = [];
+
+function GameArea(can) {
+    
+    this.canvas = can;
+    this.context = this.canvas.getContext("2d");
+    
+    this.start = function() {
+        this.interval = setInterval(updateGameArea, 20);
+    };
+    
+    this.clear = function() {
+        var image = new Image();
+        image.src = "Space.gif";
+        this.context.drawImage(image, 0, 0, this.canvas.width, this.canvas.height);
+    };
+    
+    this.stop = function() {
+        clearInterval(this.interval);
+        alert("Game Over!");
+    };
+}
