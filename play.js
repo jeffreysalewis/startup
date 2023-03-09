ba1704542da6f255dc4ea06ed2e9fc125a1e4acd
jeffreysalewis <jeffreysalewis@gmail.com>
2023-03-08 20:15:50 -0600

    added player/enemy object class

--- play.js
+++ play.js
@@ -21,3 +21,56 @@ function GameArea(can) {
         alert("Game Over!");
     };
 }
+
+function GamePiece(x, y, w, h, c, ctx, yn) {
+    var className = "GamePiece";
+    this.begx = x;
+    this.begy = y;
+    this.color = c;
+    this.addX = 0;
+    this.addY = 0;
+    this.width = w;
+    this.height = h;
+    this.yesno = yn;
+    this.context = ctx;
+    
+    this.changeAddX = function(num) {
+        this.addX += num;
+    };
+    
+    this.changeAddY = function(num) {
+        this.addY += num;
+    };
+    
+    this.draw = function() {
+        this.context.fillStyle = this.color;
+        if (this.yesno === 'n') {
+            if ((this.begx > 565) || (this.begx < 0)) {
+                this.addX *= -1;
+            }
+        }
+        this.begx += this.addX;
+        this.begy += this.addY;
+        
+        var image = new Image();
+        image.src = this.color;
+        this.context.drawImage(image, this.begx, this.begy, this.width, this.height);
+    };
+    
+    this.crashWith = function(otherobj) {
+        var myleft = this.begx;
+        var myright = this.begx + (this.width);
+        var mytop = this.begy;
+        var mybottom = this.begy + (this.height);
+        var otherleft = otherobj.begx;
+        var otherright = otherobj.begx + (otherobj.width);
+        var othertop = otherobj.begy;
+        var otherbottom = otherobj.begy + (otherobj.height);
+        var crash = false;
+        if (((mybottom > othertop) && (mytop < otherbottom)) && 
+            ((myright > otherleft) && (myleft < otherright))) {
+            crash = true;
+        }
+        return crash;
+    };
+}
