c7974119358d86266cc678c81c74c893f4529fb6
jeffreysalewis <jeffreysalewis@gmail.com>
2023-03-30 15:47:16 -0500

    updated scores to load

--- play.js
+++ play.js
@@ -112,6 +112,7 @@ function updateGameArea() {
         enemy[i].draw();
         if (player.crashWith(enemy[i])) {
             gameBoard.stop();
+            stuff.saveScore(gameBoard.score);
         } else if (laser.crashWith(enemy[i]) || laser2.crashWith(enemy[i])) {
             enemy[i].begy = Math.floor(Math.random() * ((-1 * canvas.height) + 20) - 20);
             enemy[i].begx = Math.floor(Math.random() * (canvas.width - 35));
@@ -197,5 +198,92 @@ function cambiarlasers() {
     } else {
         lasers2();
         ac = true;
+    }
+}
+class stuff{
+    async saveScore(score) {
+        const userName = this.getPlayerName();
+        const date = new Date().toLocaleDateString();
+        const newScore = { name: userName, score: score, date: date };
+    
+        try {
+          const response = await fetch('/api/score', {
+            method: 'POST',
+            headers: { 'content-type': 'application/json' },
+            body: JSON.stringify(newScore),
+          });
+    
+          //let other players know the game has ended
+          this.broadcastEvent(userName, GameEndEvent, newScore);
+    
+          // Store what the service gave us as the high scores
+          const scores = await response.json();
+          localStorage.setItem('scores', JSON.stringify(scores));
+        } catch {
+          // If there was an error then just track scores locally
+          this.updateScoresLocal(newScore);
+        }
+    }
+
+    updateScoresLocal(newScore) {
+        let scores = [];
+        const scoresText = localStorage.getItem('scores');
+        if (scoresText) {
+        scores = JSON.parse(scoresText);
+        }
+
+        let found = false;
+        for (const [i, prevScore] of scores.entries()) {
+        if (newScore > prevScore.score) {
+            scores.splice(i, 0, newScore);
+            found = true;
+            break;
+        }
+        }
+
+        if (!found) {
+        scores.push(newScore);
+        }
+
+        if (scores.length > 10) {
+        scores.length = 10;
+        }
+
+        localStorage.setItem('scores', JSON.stringify(scores));
+    }
+
+    //functionality for peer communication using websocket
+
+    configureWebSocket() {
+        const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+        this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+        this.socket.onopen = (event) => {
+        this.displayMsg('system', 'game', 'connected');
+        };
+        this.socket.onclose = (event) => {
+        this.displayMsg('system', 'game', 'disconnected');
+        };
+        this.socket.onmessage = async (event) => {
+        const msg = JSON.parse(await event.data.text());
+        if(msg.type === GameEndEvent) {
+            this.displayMsg('player', msg.from, `scored ${msg.value.score}`);
+        } else if(msg.type === GameStartEvent) {
+            this.displayMsg('player', msg.from, `started a new game`);
+        }
+        };
+    }
+
+    displayMsg(cls, from, msg) {
+        const chatText = document.querySelector('#player-messages');
+        chatText.innerHTML = `<div class="event"><span class="${cls}-event">${from}</span> ${msg}</div` + chatText.innerHTML;
+    }
+
+    broadcastEvent(from, type, value) {
+        const event = {
+        from: from,
+        type: type,
+        value: value,
+        };
+        this.socket.send(JSON.stringify(event));
     }
 }
---
+++ scores.js
@@ -0,0 +1,50 @@
+async function loadScores() {
+    let scores = [];
+    try {
+      // Get the latest high scores from the service
+      const response = await fetch('/api/scores');
+      scores = await response.json();
+  
+      // Save the scores in case we go offline in the future
+      localStorage.setItem('scores', JSON.stringify(scores));
+    } catch {
+      // If there was an error then just use the last saved scores
+      const scoresText = localStorage.getItem('scores');
+      if (scoresText) {
+        scores = JSON.parse(scoresText);
+      }
+    }
+  
+    displayScores(scores);
+  }
+  
+  function displayScores(scores) {
+    const tableBodyEl = document.querySelector('#scores');
+  
+    if (scores.length) {
+      // Update the DOM with the scores
+      for (const [i, score] of scores.entries()) {
+        const positionTdEl = document.createElement('td');
+        const nameTdEl = document.createElement('td');
+        const scoreTdEl = document.createElement('td');
+        const dateTdEl = document.createElement('td');
+  
+        positionTdEl.textContent = i + 1;
+        nameTdEl.textContent = score.name;
+        scoreTdEl.textContent = score.score;
+        dateTdEl.textContent = score.date;
+  
+        const rowEl = document.createElement('tr');
+        rowEl.appendChild(positionTdEl);
+        rowEl.appendChild(nameTdEl);
+        rowEl.appendChild(scoreTdEl);
+        rowEl.appendChild(dateTdEl);
+  
+        tableBodyEl.appendChild(rowEl);
+      }
+    } else {
+      tableBodyEl.innerHTML = '<tr><td colSpan=4>Be the first to score</td></tr>';
+    }
+  }
+  
+  loadScores();
