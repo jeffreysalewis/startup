cb4568a4a7ddd03829fa57eaa65b04450e476a76
jeffreysalewis <jeffreysalewis@gmail.com>
2023-03-30 16:22:55 -0500

    configured database for startup

--- database.js
+++ database.js
@@ -13,8 +13,8 @@ if (!userName) {
 const url = `mongodb+srv://${userName}:${password}@${hostname}`;
 
 const client = new MongoClient(url);
-const userCollection = client.db('simon').collection('user');
-const scoreCollection = client.db('simon').collection('score');
+const userCollection = client.db('startup').collection('user');
+const scoreCollection = client.db('startup').collection('score');
 
 function getUser(email) {
   return userCollection.findOne({ email: email });
