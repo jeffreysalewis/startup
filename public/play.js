c707188e32332a966aa40e81169a3e8b1cf164af
jeffreysalewis <jeffreysalewis@gmail.com>
2023-04-05 19:04:24 -0500

    debugged ws

--- public/play.js
+++ public/play.js
@@ -218,12 +218,13 @@ function cambiarlasers() {
     }
 }
 
-function getPlayerName() {
-    return localStorage.getItem('userName') ?? 'Mystery player';
-  }
 class stuff{
+    getPlayerName() {
+        return localStorage.getItem('userName') ?? 'Mystery player';
+    }
+
     async saveScore(score) {
-        const userName = getPlayerName();
+        const userName = this.getPlayerName();
         const date = new Date().toLocaleDateString();
         const newScore = { name: userName, score: score, date: date };
     
@@ -244,67 +245,67 @@ class stuff{
           // If there was an error then just track scores locally
           this.updateScoresLocal(newScore);
         }
-    }
-
-    updateScoresLocal(newScore) {
+      }
+    
+      updateScoresLocal(newScore) {
         let scores = [];
         const scoresText = localStorage.getItem('scores');
         if (scoresText) {
-        scores = JSON.parse(scoresText);
+          scores = JSON.parse(scoresText);
         }
-
+    
         let found = false;
         for (const [i, prevScore] of scores.entries()) {
-        if (newScore > prevScore.score) {
+          if (newScore > prevScore.score) {
             scores.splice(i, 0, newScore);
             found = true;
             break;
+          }
         }
-        }
-
+    
         if (!found) {
-        scores.push(newScore);
+          scores.push(newScore);
         }
-
+    
         if (scores.length > 10) {
-        scores.length = 10;
+          scores.length = 10;
         }
-
+    
         localStorage.setItem('scores', JSON.stringify(scores));
-    }
-
-    //functionality for peer communication using websocket
-
-    // configureWebSocket() {
+      }
+    
+      //functionality for peer communication using websocket
+    
+    //   configureWebSocket() {
     //     const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
     //     this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
     //     this.socket.onopen = (event) => {
-    //     this.displayMsg('system', 'game', 'connected');
+    //       this.displayMsg('system', 'game', 'connected');
     //     };
     //     this.socket.onclose = (event) => {
-    //     this.displayMsg('system', 'game', 'disconnected');
+    //       this.displayMsg('system', 'game', 'disconnected');
     //     };
     //     this.socket.onmessage = async (event) => {
-    //     const msg = JSON.parse(await event.data.text());
-    //     if(msg.type === GameEndEvent) {
+    //       const msg = JSON.parse(await event.data.text());
+    //       if(msg.type === GameEndEvent) {
     //         this.displayMsg('player', msg.from, `scored ${msg.value.score}`);
-    //     } else if(msg.type === GameStartEvent) {
+    //       } else if(msg.type === GameStartEvent) {
     //         this.displayMsg('player', msg.from, `started a new game`);
-    //     }
+    //       }
     //     };
-    // }
-
-    // displayMsg(cls, from, msg) {
+    //   }
+    
+    //   displayMsg(cls, from, msg) {
     //     const chatText = document.querySelector('#player-messages');
     //     chatText.innerHTML = `<div class="event"><span class="${cls}-event">${from}</span> ${msg}</div` + chatText.innerHTML;
-    // }
-
-    // broadcastEvent(from, type, value) {
+    //   }
+    
+    //   broadcastEvent(from, type, value) {
     //     const event = {
-    //     from: from,
-    //     type: type,
-    //     value: value,
+    //       from: from,
+    //       type: type,
+    //       value: value,
     //     };
     //     this.socket.send(JSON.stringify(event));
-    // }
+    //   }
 }
