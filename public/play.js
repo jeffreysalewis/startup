fe349e9159fe0e2cc4d8923ae29eb9c3c5f29a4a
jeffreysalewis <jeffreysalewis@gmail.com>
2023-04-05 19:18:01 -0500

    removed await

--- public/play.js
+++ public/play.js
@@ -124,7 +124,7 @@ async function updateGameArea() {
         }
         enemy[i].draw();
         if (player.crashWith(enemy[i])) {
-            await stuff.saveScore(gameBoard.score);
+            stuff.saveScore(gameBoard.score);
             gameBoard.stop();
         } else if (laser.crashWith(enemy[i]) || laser2.crashWith(enemy[i])) {
             enemy[i].begy = Math.floor(Math.random() * ((-1 * canvas.height) + 20) - 20);
