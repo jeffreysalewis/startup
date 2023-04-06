ad2b5d88681398aff91689d431cb071405cb555d
jeffreysalewis <jeffreysalewis@gmail.com>
2023-04-05 20:19:20 -0500

    removed class

--- public/play.js
+++ public/play.js
@@ -222,94 +222,92 @@ async function savarlo() {
     await stuff.saveScore(gameBoard.score);
 }
 
-class stuff{
-    getPlayerName() {
-        return localStorage.getItem('userName') ?? 'Mystery player';
+function    getPlayerName() {
+    return localStorage.getItem('userName') ?? 'Mystery player';
+}
+
+async function saveScore(score) {
+    const userName = getPlayerName();
+    const date = new Date().toLocaleDateString();
+    const newScore = { name: userName, score: score, date: date };
+
+    try {
+        const response = await fetch('/api/score', {
+        method: 'POST',
+        headers: { 'content-type': 'application/json' },
+        body: JSON.stringify(newScore),
+        });
+
+        //let other players know the game has ended
+        //this.broadcastEvent(userName, GameEndEvent, newScore);
+
+        // Store what the service gave us as the high scores
+        const scores = await response.json();
+        localStorage.setItem('scores', JSON.stringify(scores));
+    } catch {
+        // If there was an error then just track scores locally
+        this.updateScoresLocal(newScore);
+    }
+}
+
+function updateScoresLocal(newScore) {
+    let scores = [];
+    const scoresText = localStorage.getItem('scores');
+    if (scoresText) {
+        scores = JSON.parse(scoresText);
     }
 
-    async saveScore(score) {
-        const userName = this.getPlayerName();
-        const date = new Date().toLocaleDateString();
-        const newScore = { name: userName, score: score, date: date };
-    
-        try {
-          const response = await fetch('/api/score', {
-            method: 'POST',
-            headers: { 'content-type': 'application/json' },
-            body: JSON.stringify(newScore),
-          });
-    
-          //let other players know the game has ended
-          //this.broadcastEvent(userName, GameEndEvent, newScore);
-    
-          // Store what the service gave us as the high scores
-          const scores = await response.json();
-          localStorage.setItem('scores', JSON.stringify(scores));
-        } catch {
-          // If there was an error then just track scores locally
-          this.updateScoresLocal(newScore);
+    let found = false;
+    for (const [i, prevScore] of scores.entries()) {
+        if (newScore > prevScore.score) {
+        scores.splice(i, 0, newScore);
+        found = true;
+        break;
         }
-      }
-    
-      updateScoresLocal(newScore) {
-        let scores = [];
-        const scoresText = localStorage.getItem('scores');
-        if (scoresText) {
-          scores = JSON.parse(scoresText);
-        }
-    
-        let found = false;
-        for (const [i, prevScore] of scores.entries()) {
-          if (newScore > prevScore.score) {
-            scores.splice(i, 0, newScore);
-            found = true;
-            break;
-          }
-        }
-    
-        if (!found) {
-          scores.push(newScore);
-        }
-    
-        if (scores.length > 10) {
-          scores.length = 10;
-        }
-    
-        localStorage.setItem('scores', JSON.stringify(scores));
-      }
-    
-      //functionality for peer communication using websocket
-    
-    //   configureWebSocket() {
-    //     const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
-    //     this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
-    //     this.socket.onopen = (event) => {
-    //       this.displayMsg('system', 'game', 'connected');
-    //     };
-    //     this.socket.onclose = (event) => {
-    //       this.displayMsg('system', 'game', 'disconnected');
-    //     };
-    //     this.socket.onmessage = async (event) => {
-    //       const msg = JSON.parse(await event.data.text());
-    //       if(msg.type === GameEndEvent) {
-    //         this.displayMsg('player', msg.from, `scored ${msg.value.score}`);
-    //       } else if(msg.type === GameStartEvent) {
-    //         this.displayMsg('player', msg.from, `started a new game`);
-    //       }
-    //     };
-    //   }
-    
-    //   displayMsg(cls, from, msg) {
-    //     const chatText = document.querySelector('#player-messages');
-    //     chatText.innerHTML = `<div class="event"><span class="${cls}-event">${from}</span> ${msg}</div` + chatText.innerHTML;
-    //   }
-    
-    //   broadcastEvent(from, type, value) {
-    //     const event = {
-    //       from: from,
-    //       type: type,
-    //       value: value,
-    //     };
-    //     this.socket.send(JSON.stringify(event));
-    //   }
+    }
+
+    if (!found) {
+        scores.push(newScore);
+    }
+
+    if (scores.length > 10) {
+        scores.length = 10;
+    }
+
+    localStorage.setItem('scores', JSON.stringify(scores));
 }
+
+    //functionality for peer communication using websocket
+
+//   configureWebSocket() {
+//     const protocol = window.location.protocol === 'http:' ? 'ws' : 'wss';
+//     this.socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
+//     this.socket.onopen = (event) => {
+//       this.displayMsg('system', 'game', 'connected');
+//     };
+//     this.socket.onclose = (event) => {
+//       this.displayMsg('system', 'game', 'disconnected');
+//     };
+//     this.socket.onmessage = async (event) => {
+//       const msg = JSON.parse(await event.data.text());
+//       if(msg.type === GameEndEvent) {
+//         this.displayMsg('player', msg.from, `scored ${msg.value.score}`);
+//       } else if(msg.type === GameStartEvent) {
+//         this.displayMsg('player', msg.from, `started a new game`);
+//       }
+//     };
+//   }
+
+//   displayMsg(cls, from, msg) {
+//     const chatText = document.querySelector('#player-messages');
+//     chatText.innerHTML = `<div class="event"><span class="${cls}-event">${from}</span> ${msg}</div` + chatText.innerHTML;
+//   }
+
+//   broadcastEvent(from, type, value) {
+//     const event = {
+//       from: from,
+//       type: type,
+//       value: value,
+//     };
+//     this.socket.send(JSON.stringify(event));
+//   }
