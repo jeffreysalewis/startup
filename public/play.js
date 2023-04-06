97c846cf92a301f6e4757737d8f7622c02975a35
jeffreysalewis <jeffreysalewis@gmail.com>
2023-04-05 20:44:46 -0500

    loading score at gameover

--- public/play.js
+++ public/play.js
@@ -113,7 +113,7 @@ function main() {
 }
 
 //"animates" the canvas, moves the player and enemies and lasers
-function updateGameArea() {
+async function updateGameArea() {
     gameBoard.clear();
     var canvas = document.getElementById("myCanvas");
     for (var i = 0; i < 7; i++) {
@@ -124,7 +124,7 @@ function updateGameArea() {
         }
         enemy[i].draw();
         if (player.crashWith(enemy[i])) {
-            //stuff.saveScore(gameBoard.score);
+            await saveScore(gameBoard.score);
             gameBoard.stop();
         } else if (laser.crashWith(enemy[i]) || laser2.crashWith(enemy[i])) {
             enemy[i].begy = Math.floor(Math.random() * ((-1 * canvas.height) + 20) - 20);
